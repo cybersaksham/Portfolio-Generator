65eeae8462b068cb78e6a24b11ba7ef65f1d32f7
Saksham Bindal <saksham.bindal2004@gmail.com>
2022-12-28 19:36:32 +0530

    counter data

--- Required Code/Data/Counter.js
+++ Required Code/Data/Counter.js
@@ -1,31 +1,3 @@
-let currDate = new Date();
-let year = currDate.getFullYear() - 2020;
-
-const counterItems = [
-  {
-    icon: "bi bi-globe",
-    count: 50,
-    duration: 0.5,
-    title: "Websites",
-  },
-  {
-    icon: "bi bi-phone",
-    count: 10,
-    duration: 0.5,
-    title: "Android Apps",
-  },
-  {
-    icon: "bi bi-currency-bitcoin",
-    count: 3,
-    duration: 0.5,
-    title: "Web 3.0 Apps",
-  },
-  {
-    icon: "bi bi-lightbulb-fill",
-    count: year,
-    duration: 0.5,
-    title: "Years of Experience",
-  },
-];
+const counterItems = [[items]];
 
 export default counterItems;
