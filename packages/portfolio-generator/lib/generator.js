cbc0534a9a6c2f73dc6bd7c6ed3232d4a302ddf1
Saksham Bindal <saksham.bindal2004@gmail.com>
2022-12-28 14:51:58 +0530

    check app name

--- packages/portfolio-generator/lib/generator.js
+++ packages/portfolio-generator/lib/generator.js
@@ -1,5 +1,8 @@
-const { showWarning } = require("cybersaksham-npm-logs");
+const { showWarning, showError } = require("cybersaksham-npm-logs");
+const validateProjectName = require("validate-npm-package-name");
 const { checkNodeVersion } = require("./versions");
+const path = require("path");
+const chalk = require("chalk");
 
 module.exports.createApp = (name, version) => {
   if (!checkNodeVersion()) {
@@ -12,5 +15,38 @@ module.exports.createApp = (name, version) => {
     });
     // Fall back to latest supported react-scripts on Node 4
     version = "react-scripts@0.9.x";
+  }
+
+  const root = path.resolve(name);
+  const appName = path.basename(root);
+  checkAppName(appName);
+};
+
+const checkAppName = (appName) => {
+  const validationResult = validateProjectName(appName);
+  if (!validationResult.validForNewPackages) {
+    // Show Errors
+    let errors = [
+      `Cannot create a project named ${chalk.green(
+        `"${appName}"`
+      )} because of npm naming restrictions:`,
+    ];
+    let validationErrors = [
+      ...(validationResult.errors || []),
+      ...(validationResult.warnings || []),
+    ];
+    if (validationErrors.length > 0) {
+      errors.push("Found errors are:");
+      validationErrors.forEach((err) => {
+        errors.push(chalk.red(`  *${err}`));
+      });
+    }
+    showError({
+      code: 400,
+      errors,
+      summary: ["Please choose a different project name."],
+    });
+
+    process.exit(1);
   }
 };
