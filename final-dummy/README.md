de21f2006385ea6c15d9085abecf35ffe051c7cc
Saksham Bindal <saksham.bindal2004@gmail.com>
2022-12-31 20:07:00 +0530

    final dummy website removed

--- final-dummy/README.md
+++
@@ -1,4 +0,0 @@
-# Portfolio Website
-
-<p>This website is my portfolio website made in Next JS.</p>
-<p>Visit <a href="https://cybersaksham.co.in">here</a></p>
--- final-dummy/pages/detail/[id].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-import Detail from "../../Components/Detail";
-
-export default function DetailPage() {
-  const router = useRouter();
-
-  return <Detail id={router.query.id} />;
-}
