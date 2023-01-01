6c5d644afe04e9b652d849f669027e909fafff15
Saksham Bindal <saksham.bindal2004@gmail.com>
2023-01-01 15:19:16 +0530

    config

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
     [
       "@docusaurus/plugin-pwa",
       {
-        debug: true,
+        debug: false,
         offlineModeActivationStrategies: [
           "appInstalled",
           "standalone",
