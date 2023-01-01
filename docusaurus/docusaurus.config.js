275c9ec11765089b01c454d3c67d9b3780946bf5
Saksham Bindal <saksham.bindal2004@gmail.com>
2023-01-01 14:25:36 +0530

    pwa integration

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -51,6 +51,69 @@ const config = {
     ],
   ],
 
+  plugins: [
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
+          "mobile",
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/icons/500x500.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#000",
+          },
+          {
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "/img/icons/500x500.png",
+          },
+          {
+            tagName: "link",
+            rel: "mask-icon",
+            href: "/img/icons/500x500.png",
+            color: "rgb(62, 204, 94)",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileImage",
+            content: "/img/icons/500x500.png",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileColor",
+            content: "#000",
+          },
+        ],
+      },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -64,7 +127,7 @@ const config = {
         title: appData.appName,
         logo: {
           alt: `${appData.appName} Logo`,
-          src: "img/logo.svg",
+          src: "img/icons/500x500.png",
         },
         items: [
           { to: appData.docURL, label: "Docs", position: "left" },
